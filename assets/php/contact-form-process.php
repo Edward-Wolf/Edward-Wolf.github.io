b45a623808f8f9ced4d67b58e08dd83d76b30a6e
Edward Wolf <103476112+Edward-Wolf@users.noreply.github.com>
2022-06-04 12:16:01 -0400

    Update contact-form-process.php
    Created two header arrays to send the original email and a copy of the email. Wrapped the 'mail' calls in if statements to track submission of the form.

--- assets/php/contact-form-process.php
+++ assets/php/contact-form-process.php
@@ -1,4 +1,9 @@
 <?php 
+
+error_reporting(-1);
+ini_set('display_errors', 'On');
+set_error_handler("var_dump");
+
 if(isset($_POST['submit'])){
     $to = "edward.james.wolf@gmail.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
@@ -8,10 +13,24 @@ if(isset($_POST['submit'])){
     $message = $name . " wrote the following:" . "\n\n" . $_POST['comment'];
     $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['comment'];
 
-    $headers = "From:" . $from;
-    $headers2 = "To:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    header('Location: http://www.edward-wolf.com/index.html');
+    $headers = array("From: " . $from,
+    "Reply-To: " . $to,
+    "X-Mailer: PHP/" . PHP_VERSION
+    );
+    $headers = implode("\r\n", $headers);
+    
+    $headers2 = array("From: " . $to,
+    "Reply-To: " . $from,
+    "X-Mailer: PHP/" . PHP_VERSION
+    );
+    $headers2 = implode("\r\n", $headers2);
+    
+    if(mail($to,$subject,$message,$headers)){
+        mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+        header('Location: http://www.edward-wolf.com/index.html');
+    } else {
+        echo '<p>There was an error. We apologize for the inconvenience.</p>';
+    }
+
     }
 ?>
